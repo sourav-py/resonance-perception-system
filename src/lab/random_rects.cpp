47f52306a2a5904db4909eeb5884b8b239b39cb3
sourav-py <imsrv2k@gmail.com>
2026-04-05 23:43:32 +0530

    lab: write a test program to generate rectangles with random dimensions and positions at regular intervals

---
+++ src/lab/random_rects.cpp
@@ -0,0 +1,92 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_video.h>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+/*
+ * This is an in-progress module.
+ * Do not execute the program yet.
+ * Need some optimizations/corrections before doing so.
+ */
+
+int main(){
+  SDL_Window *window = SDL_CreateWindow("White board", 0, 0, 1280, 720, SDL_WINDOW_FULLSCREEN_DESKTOP);
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+  //Set background to white
+  SDL_SetRenderDrawColor(renderer, 191, 191, 191, 255);
+  SDL_RenderClear(renderer);
+
+  bool running = true;
+  SDL_Event event;
+
+  //Define an array of rectangles
+  vector<SDL_Rect> rectangles;
+
+  //Seed the random generator with time
+  srand(time(0));
+
+  Uint32 checkPointTime = SDL_GetTicks();
+
+  while(running){
+
+    while(SDL_PollEvent(&event))
+    {
+      if(event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE)
+      {
+	running = false;
+      }
+    }
+
+    //Check if 3 seconds have elapsed
+    Uint32 currTime = SDL_GetTicks();
+    if(currTime - checkPointTime >= 2000)
+    {
+      // Generate random dimenstions
+      int x = 20 + rand()%150;
+      int y = 20 + rand()%150;
+
+      //Generate random position
+      int p1 = rand()%1280;
+      int p2 = rand()%720;
+
+      SDL_Rect rect = {p1, p2, x, y};
+      rectangles.push_back(rect);
+
+      //Update the checkpoint
+      checkPointTime = currTime;
+    }
+    
+
+
+    
+    //Clear the screen - set the color to white
+    SDL_SetRenderDrawColor(renderer, 191, 191, 191, 191);
+    SDL_RenderClear(renderer);
+
+    //Set Render color to black (to draw rectangles)
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
+
+    //Render each rectangle present in the array
+    for(auto rect : rectangles)
+      SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
+  }
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  return 0;
+}
